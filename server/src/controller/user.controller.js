33948fc4c3e373fc5605c6fcafd6810042b42b83
codevskills <ccschools.edu@gmail.com>
2023-11-03 09:21:17 +0530

    N

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -30,6 +30,12 @@ router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
   console.log(req.body);
 
+  if (!username || !email || !password) {
+    return res
+      .status(400)
+      .json({ message: "All fields required" });
+  }
+
   try {
     const existingUser = await userDataModel.findOne({ email });
 
