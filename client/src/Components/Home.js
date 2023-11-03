fb0191047cf42202dd5793182a0d2b6d6b240a5f
codevskills <ccschools.edu@gmail.com>
2023-11-03 09:45:29 +0530

    New

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Button } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
 import { LogoutHandleDeclaration } from "./Redux/ReduxUserData/UserDataAction";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const userEmail = useSelector((state) => state.userData.userEmail);
   console.log(userEmail);
 
@@ -13,14 +14,46 @@ const Home = () => {
     dispatch(LogoutHandleDeclaration());
   };
 
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
   return (
     <div>
-      <h3 className="text-center">Welcome to Home</h3>
-      <Button onClick={handleLogout}>Logout</Button>
-      <Link to={"/login"}>
-        <Button>Login</Button>
-      </Link>
-      {userEmail ? <p>Video</p> : <p>Pay Now to get video subscription</p>}
+      <Row
+        className="d-flex py-2 bg-dark text-light"
+        bg="dark"
+        data-bs-theme="dark"
+      >
+        <Col className="d-flex">
+          <div className="w-100">
+            <h3 className="text-center">Welcome to Home</h3>
+          </div>
+          <span className="d-flex gap-3 mx-3">
+            <Button variant="light" onClick={handleLogout}>
+              Logout
+            </Button>
+            <Button variant="light" onClick={handleLogin}>
+              Login
+            </Button>
+          </span>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex justify-content-center">
+          {userEmail ? (
+            <p>
+              <img
+                src="https://www.hubspot.com/hubfs/Smiling%20Leo%20Perfect%20GIF.gif"
+                alt="..."
+                className="rounded mt-5"
+              />
+            </p>
+          ) : (
+            <p>Pay Now to get video subscription</p>
+          )}
+        </Col>
+      </Row>
     </div>
   );
 };
